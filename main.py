f10544a14f46b51f32334a35ef3c6d0229313d17
kimstitute <kimstitute@naver.com>
2025-09-18 10:33:04 +0900

    환경 변수 불러오기 코드 위치 수정 및 os 모듈 추가

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 import streamlit as st
 from openai import OpenAI
+import os
+from dotenv import load_dotenv
 
 st.title("자동 보고서 생성기 \nCreated by Ctrl+Z")
 subject = st.text_input("어느 지역에 대한 보고서를 작성할까요?")
 st.write(f"지역명: {subject}")
-
-from dotenv import load_dotenv
 
 # 환경 변수 불러오기
 load_dotenv()
