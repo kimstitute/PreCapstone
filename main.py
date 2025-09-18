0affa505311f52c73e13655b8cb4605d1fdb8413
kimstitute <kimstitute@naver.com>
2025-09-18 10:19:42 +0900

    haha

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 import streamlit as st
 from openai import OpenAI
 
-client = OpenAI(api_key=st.secrets["OPENAI_API_KEY"])
-
-st.title("자동 보고서 생성기 Created by Ctrl+Z")
+st.title("자동 보고서 생성기 \nCreated by Ctrl+Z")
 subject = st.text_input("어느 지역에 대한 보고서를 작성할까요?")
 st.write(f"지역명: {subject}")
 
 if st.button("보고서 작성"):
+    api_key = st.secrets["OPENAI_API_KEY"]
+    client = OpenAI(api_key=api_key) 
     with st.spinner("보고서 작성 중..."):
         response = client.chat.completions.create(
             model="gpt-4o-mini",
